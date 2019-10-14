93084f4c61da958dbe334f5b0cd1e847bf8d7519
Tom Trogdon <trogdon@uw.edu>
2019-10-14 15:48:53 -0700

    Initial project files using PkgTemplates

---
+++ src/ApproxFunRational.jl
@@ -0,0 +1,5 @@
+module ApproxFunRational
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ApproxFunRational
+using Test
+
+@testset "ApproxFunRational.jl" begin
+    # Write your own tests here.
+end
