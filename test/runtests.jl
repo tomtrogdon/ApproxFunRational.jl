fcbf8b1adbede1d565e9db3bb2a0bfd152096d64
Tom Trogdon <trogdon@uw.edu>
2019-10-23 09:04:35 -0700

    Clean up test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,9 +23,36 @@ using Test
     @test F(.1) ≈ f(.1)*exp(β*im*.1)
     H = F*G
     @test H(.1) ≈ F(.1)*G(.1)
+
+    L = 0.5; α = -2.; β = 2.;
+    dom = PeriodicLine{false,Float64}(0.,L)
+    g = z -> sech(z)
+    f = z -> exp(-z^2+z)
+    G = Fun(zai(g), OscLaurent(dom,α), 400)
+    @test G(.1) ≈ g(.1)*exp(α*im*.1)
+    F = Fun(zai(f), OscLaurent(dom,β), 400)
+    @test F(.1) ≈ f(.1)*exp(β*im*.1)
+    H = F*G
+    @test H(.1) ≈ F(.1)*G(.1)
 end
 
 @testset "ApproxFunRational.jl: Differentiation and integration" begin
+    L = 1.; α = -2.; β = 2.;
+    dom = PeriodicLine{false,Float64}(0.,L)
+    g = z -> sech(z)
+    f = z -> exp(-z^2+z)
+    df = z-> (1-2z)*exp(-z^2+z)
+    G = Fun(zai(g), OscLaurent(dom,α), 200)
+    F = Fun(zai(f), OscLaurent(dom,β), 200)
+    H = F*G
+    dF = F'
+    dF_2 = Derivative()*F
+    @test dF(.1) ≈ df(.1)*exp(β*im*.1) + (β*im)*f(.1)*exp(β*im*.1)
+    @test dF_2(.1) ≈ df(.1)*exp(β*im*.1) + (β*im)*f(.1)*exp(β*im*.1)
+    @test sum(H) ≈ sqrt(pi)
+    @test sum(G) ≈ pi*sech(1.0*pi)
+    @test sum(F) ≈ exp(-3/4 + im)*sqrt(pi)
+
     L = 2.; α = -2.; β = 2.;
     dom = PeriodicLine{false,Float64}(0.,L)
     g = z -> sech(z)
@@ -41,43 +68,20 @@ end
     @test sum(H) ≈ sqrt(pi)
     @test sum(G) ≈ pi*sech(1.0*pi)
     @test sum(F) ≈ exp(-3/4 + im)*sqrt(pi)
+
+    L = 0.5; α = -2.; β = 2.;
+    dom = PeriodicLine{false,Float64}(0.,L)
+    g = z -> sech(z)
+    f = z -> exp(-z^2+z)
+    df = z-> (1-2z)*exp(-z^2+z)
+    G = Fun(zai(g), OscLaurent(dom,α), 400)
+    F = Fun(zai(f), OscLaurent(dom,β), 400)
+    H = F*G
+    dF = F'
+    dF_2 = Derivative()*F
+    @test dF(.1) ≈ df(.1)*exp(β*im*.1) + (β*im)*f(.1)*exp(β*im*.1)
+    @test dF_2(.1) ≈ df(.1)*exp(β*im*.1) + (β*im)*f(.1)*exp(β*im*.1)
+    @test sum(H) ≈ sqrt(pi)
+    @test sum(G) ≈ pi*sech(1.0*pi)
+    @test sum(F) ≈ exp(-3/4 + im)*sqrt(pi)
 end
-
-
-
-j = -2
-L = 1.
-p = 1im*L
-f = z -> ((p-z)/(z+p))^j - (-1)^j
-df = z -> j*((p-z)/(p+z))^(j-1)*(-2p)/(p+z)^2
-dom = PeriodicLine{false,Float64}(0.,L)
-
-F = Fun(zai(f), OscLaurent(dom,0.), 10)
-F.coefficients
-dF = Derivative()*F
-ApproxFunRational.fsum(dF.coefficients)
-
-dF(.1)
-df(.1)
-
-
-
-
-
-
-
-
-B[:,3]
-ApproxFunRational.mob_derivative_getindex(dom,1,4,2)
-
-
-
-
-dF_true = Fun(zai(df), OscLaurent(dom,0.), 8)
-dF_true.coefficients
-
-
-
-
-dF_true(.1)
-dF(.1)
